ae923b986ec1f65ba287975c859902cfe9cca6cb
stalewhitebread <jcriddle@una.edu>
2024-01-24 20:56:04 -0600

    added port binding

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5500;
 
 app.get('/', function (req, res) {
   
-    res.send('Hello World from express')
+    res.send('Hello World from Awesome-O. Awesome-O is type achiever, so Awesome-O can be too competitive sometimes. Awesome-O is nothing but "authentic", however.')
 })
+//app.listen(3000)
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`Michael listening on port ${port}`)
+})
